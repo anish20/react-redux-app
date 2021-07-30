e557d3e4ae4d3a0151dc4db3da178ac43bdc14bf
Mohd Anish <anish15.786@gmail.com>
2021-07-30 15:02:15 +0530

    add container to connect react and redux

--- src/containers/HomeContainers.js
+++ src/containers/HomeContainers.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addToCart,removeToCart} from '../services/actions/actions'
 import Home from '../components/Home';
 
-export default Home;
+const mapSateToProps=sate=>({
+
+})
+
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+
+
+
+
+export default   connect(mapSateToProps,mapDispatchToProps)(Home);
